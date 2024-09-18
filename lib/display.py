7240bedfe62b0b87a0015363f6994b5364c83e9f
Praveen <praveen.x.verma@gmail.com>
2024-09-19 01:11:18 +0530

    Baic code for camera

---
+++ lib/display.py
@@ -0,0 +1,366 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#import chardet
+import os
+import sys 
+import time
+import logging
+import spidev as SPI
+#sys.path.append("..")
+import RPi.GPIO as GPIO
+import shutil
+import lib.LCD_2inch as LCD_2inch
+from PIL import Image,ImageDraw,ImageFont
+
+# Raspberry Pi pin configuration:
+RST = 27
+DC = 23
+BL = 18
+bus = 0 
+device = 0 
+#logging.basicConfig(level=logging.DEBUG)
+width = 320
+height = 240
+disp = LCD_2inch.LCD_2inch()
+
+# Variables for display/screen
+brightness = 50
+SPI_SPEED_MHZ = 80
+exposure_time=["Auto","1/1600","1/1250","1/1000","1/800","1/640","1/500","1/400","1/320","1/250","1/200","1/160","1/125","1/100","1/80","1/60","1/50","1/40","1/30","1/25","1/20","1/15","1/13","1/10","1/8","1/6","1/5","1/4","1/3","1/2.5","1/2","1/1.6","1/1.3",'1"','1.3"','1.6"','2"','2.5"','3"','4"','5"','6"','8"','10"','13"','15"','20"','25"','30"','60"']
+
+image_h=[1600,2048,2464,3008,3264,3888,4000,4656]
+image_w=[1200,1536,1632,2000,2448,2592,2800,3496]
+
+noise_reduction_mode = ("Off","Fast","Quality")
+white_balance = ("Auto","Tungsten","Fluorescent","Indoor","Daylight","Cloudy")
+
+# Define colors
+RED=(255,0,0)
+GREEN=(0,200,0)
+BLACK=(0,0,0)
+GRAY=(120,120,120)
+INFO=(200,200,175)
+WHITE=(255,255,255)
+YELLOW=(255,255,0)
+MENU_SELECT=(200,200,200)
+MENU_TEXT=(10,200,200)
+MENU_TITLE=(128,255,10)
+
+# Path for fonts
+# Fonts used are taken from the waveshare LCD interface example code
+# URL : https://www.waveshare.com/w/upload/a/a8/LCD_Module_RPI_code.7z
+boot_screen = ImageFont.truetype("./font/Font01.ttf",50)
+menu_icon_large = ImageFont.truetype("./font/Font02.ttf",30)
+menu_icon = ImageFont.truetype("./font/Font02.ttf",24)
+home_info = ImageFont.truetype("./font/Font02.ttf",19)
+menu_title = ImageFont.truetype("./font/Font02.ttf",30)
+menu_line = ImageFont.truetype("./font/Font02.ttf",24)
+
+# LCD : Show image_file on screen
+def show_image(image):
+    basewidth = width
+    wpercent = (basewidth/float(image.size[0]))
+    hsize = int((float(image.size[1])*float(wpercent)))
+    image= image.resize((basewidth,hsize), resample=Image.BICUBIC)
+    new_image=Image.new("RGB",(width,height),color='black')
+    new_image.paste(image,(0,0))
+    disp.ShowImage(new_image)
+
+def draw_bar(image,value,background_color=GRAY,bar_fill_color=WHITE):
+    draw = ImageDraw.Draw(image)
+    # Draw a handy on-screen bar to show us the current brightness
+    bar_width = int((225 / 100.0) * value)
+    draw.rectangle((10,216,230,216), background_color)
+    draw.rectangle((10,216,5+bar_width,216), bar_fill_color)
+    # return the resulting image
+    return image
+
+def progress_bar(image_file,x,shoot_config,camera_config,background_color=GRAY,bar_fill_color=WHITE):
+    basewidth = width
+    image_count = 1
+    image = Image.open(image_file)
+    wpercent = (basewidth/float(image.size[0]))
+    hsize = int((float(image.size[1])*float(wpercent)))
+    image= image.resize((basewidth,hsize), resample=Image.BICUBIC)
+    new_image=Image.new("RGB",(width,height),color='black')
+    new_image.paste(image,(0,30))
+    draw = ImageDraw.Draw(new_image)
+    # Draw a handy on-screen bar to show us the current brightness
+    info_color = MENU_TITLE
+    if(camera_config["bnw"]):
+        info_color = INFO
+    if(camera_config["exposure"] == 0):
+        draw.text((165,1),exposure_time[camera_config["exposure"]],fill = MENU_TITLE, font = home_info)
+    else:
+        draw.text((165,1),exposure_time[camera_config["exposure"]],fill = MENU_TEXT, font = home_info)
+    if( shoot_config["shoot_mode"] == 1 ):                # mode 1 - stills
+        draw.text((5,1),"PIC", fill = info_color,font = home_info)
+    elif( shoot_config["shoot_mode"] == 2 ):              # mode 2 - bracketing
+        draw.text((5,1),"BKT", fill = info_color,font = home_info)
+        image_count = shoot_config["bkt_frame_count"]
+    elif( shoot_config["shoot_mode"] == 3 ):              # mode 3 - timelapse stills
+        draw.text((5,1),"InT", fill = info_color,font = home_info)
+        image_count = shoot_config["tlp_frame_count"]
+    elif( shoot_config["shoot_mode"] == 4 ):              # mode 4 - timelapse video
+        draw.text((5,1),"TLV", fill = info_color,font = home_info)
+        image_count = shoot_config["tlv_frame_count"]
+    if (camera_config["raw"]):
+        draw.text((40,1),"RAW", fill = INFO,font = home_info)
+    else:
+        draw.text((40,1),"JPG", fill = RED,font = home_info)
+    draw.text((80,1),("ISO "+str(int(camera_config["analogue_gain"] * 100))), fill = MENU_TEXT,font = home_info)
+    value = int(((x+1)/image_count)*100)
+    bar_width = int((225 / 100.0) * value)
+    draw.text((5,215),"Processing → "+str(x+1)+" / "+str(image_count), fill = YELLOW,font = home_info)
+    draw.rectangle((10,216,230,216), background_color)
+    draw.rectangle((10, 216,5+bar_width, 216), bar_fill_color)
+    disp.ShowImage(new_image)
+
+#############################################################################
+# Functions to print specific menu options for camera
+#############################################################################
+
+def boot_disp(file_name):
+    # Initialize library.
+    disp.Init()
+    # Clear display.
+    disp.clear()
+
+    dir= "logo"
+    camera_logo_file= os.path.join(os.getcwd(),dir,file_name)
+    image=Image.open(camera_logo_file)
+    image=image.resize((width,height), resample=Image.BICUBIC)
+    draw = ImageDraw.Draw(image)
+    disp.ShowImage(image)
+
+def menu_display(header,menu_item,display_config,bar_value=0):
+    item_number = display_config["menu"] % 10
+    image=Image.new("RGB",(width,height),color='black')
+    draw = ImageDraw.Draw(image)
+################# Interface Buttons #########################
+    if(display_config["left"]):
+        draw.text((80,215),"<", fill = RED,font = menu_icon)
+    if(display_config["right"]):
+        draw.text((145,215),">",fill = RED,font = menu_icon)
+    if(display_config["down"]):
+        draw.text((5,215),"↓", fill = RED,font = menu_icon_large)
+    if(display_config["up"]):
+        draw.text((212,215),"↑", fill = RED,font = menu_icon_large)
+#############################################################
+    draw.text((0,5),header,fill = MENU_TITLE,font = menu_title)
+    count = 0
+    for item in menu_item:
+        if(item_number == (count+1)):
+            draw.rectangle([(230,(36+(25*(count+1)))),(10,(36+(25*count)))],fill = MENU_SELECT)
+            draw.text((15,(33+(25*count))),item,fill = BLACK,font = menu_line)
+        else:
+            draw.text((15,(33+(25*count))),item,fill = MENU_TEXT,font = menu_line)
+        count += 1
+    if(display_config["menu"] == 41):
+        st7789.display(draw_bar(image,display_config["brightness"]))
+    elif(display_config["menu"] == 42):
+        if(bar_value < 50):
+            COLOR = GREEN
+        elif(bar_value >= 50 and bar_value <=75):
+            COLOR = YELLOW
+        else:
+            COLOR = RED
+        disp.ShowImage(draw_bar(image,bar_value,bar_fill_color=COLOR))
+    disp.ShowImage(image)
+
+def camera_home(display_config,shoot_config,camera_config,preview_image):
+    preview_image= preview_image.resize((288,216), resample=Image.BICUBIC)
+    image=Image.new("RGB",(width,height),color='black')
+    image.paste(preview_image,(16,24))
+    draw = ImageDraw.Draw(image)
+    info_color = MENU_TITLE
+    if(camera_config["bnw"]):
+        info_color = INFO
+    if(camera_config["exposure"] == 0):
+        draw.text((165,1),exposure_time[camera_config["exposure"]],fill = MENU_TITLE, font = home_info)
+    else:
+        draw.text((165,1),exposure_time[camera_config["exposure"]],fill = MENU_TEXT, font = home_info)
+    if( shoot_config["shoot_mode"] == 1 ):                # mode 1 - stills
+        draw.text((5,1),"PIC", fill = info_color,font = home_info)
+    elif( shoot_config["shoot_mode"] == 2 ):              # mode 2 - bracketing
+        draw.text((5,1),"BKT", fill = info_color,font = home_info)
+    elif( shoot_config["shoot_mode"] == 3 ):              # mode 3 - timelapse stills
+        draw.text((5,1),"InT", fill = info_color,font = home_info)
+    elif( shoot_config["shoot_mode"] == 4 ):              # mode 4 - timelapse video
+        draw.text((5,1),"TLV", fill = info_color,font = home_info)
+    if (camera_config["raw"]):
+        draw.text((40,1),"RAW", fill = INFO,font = home_info)
+    else:
+        draw.text((40,1),"JPG", fill = RED,font = home_info)
+    draw.text((80,1),("ISO "+str(int(camera_config["analogue_gain"] * 100))), fill = MENU_TEXT,font = home_info)
+    if(display_config["left"]):
+        draw.text((80,215),"<", fill = RED,font = menu_icon)
+    if(display_config["right"]):
+        draw.text((145,215),">",fill = RED,font = menu_icon)
+    disp.ShowImage(image)
+
+def menu_control(display_config,shoot_config,camera_config):
+    menu = display_config.get("menu")
+    if( menu >= 1 and menu <= 9):
+        items=["Camera Control","Shooting Mode","Image Settings","System Menu","Power Options"]
+        menu_display("Menu",items,display_config)
+
+    elif( menu >= 11 and menu <= 19 ):        # Camera control settings
+        items=["Exposure","ISO","Contrast","Sharpness","Noise Reduction","AWB"]
+        if(shoot_config["shoot_mode"] == 1):
+            menu_page_title = "Photo Mode Setup"
+        elif(shoot_config["shoot_mode"] == 2):
+            menu_page_title = "Bracketing Mode Setup"
+        elif(shoot_config["shoot_mode"] == 3):
+            menu_page_title = "Interval Timer Setup"
+        elif(shoot_config["shoot_mode"] == 4):
+            menu_page_title = "Timelapse Movie Setup"
+        items[0] = items[0] + " → " + exposure_time[camera_config["exposure"]]
+        items[1] = items[1] + " → " + str(int(camera_config["analogue_gain"] * 100))
+        items[2] = items[2] + " → " + str(camera_config["contrast"])
+        items[3] = items[3] + " → " + str(camera_config["sharpness"])
+        items[4] = items[4] + " → " + noise_reduction_mode[camera_config["noise_reduction"]]
+        items[5] = items[5] + " → " + white_balance[camera_config["white_balance"]]
+        menu_display(menu_page_title,items,display_config)
+
+    elif( menu >= 21 and menu <= 29 ):        # Shooting mode
+        items=["Photo","Bracketing","Interval Timer Shoot","Timelapse Movie"]
+        menu_display("Shooting Mode",items,display_config)
+
+    elif( menu >= 222 and menu <= 229 ):      # Bracketing submenu
+        items=["Photo","Bracketing","* Frames","Interval Timer Shoot","Timelapse Movie"]
+
+        items[2]=items[2] + " → " + str(shoot_config["bkt_frame_count"])
+        menu_display("Shooting Mode",items,display_config)
+
+    elif( menu >= 233 and menu <= 239 ):      # Timelapse photo submenu
+        items=["Photo","Bracketing","Interval Timer Shoot","* Frames","* Interval","Timelapse Movie"]
+        items[3]=items[3] + " → " + str(shoot_config["tlp_frame_count"])
+        items[4]=items[4] + " → " + str(shoot_config["tlp_interval"])
+        menu_display("Shooting Mode",items,display_config)
+
+    elif( menu >= 244 and menu <= 249 ):      # Timelapse video submenu
+        items=["Photo","Bracketing","Interval Timer Shoot","Timeplapse Movie","* Frames","* Interval"]
+        items[4]=items[4] + " → " + str(shoot_config["tlv_frame_count"])
+        items[5]=items[5] + " → " + str(shoot_config["tlv_interval"])
+        menu_display("Shooting Mode",items,display_config)
+
+    elif( menu >= 31 and menu <= 39 ):        # Image Setting Menu
+        items=["Size","Output","File","Status LED"]
+        items[0] = items[0] + " → " + str(image_h[camera_config["image_size"]]) +" X " + str(image_w[camera_config["image_size"]])
+        if(camera_config["bnw"]):
+            items[1] += " → B & W"
+        else:
+            items[1] += " → COLOR"
+        if(camera_config["raw"]):
+            items[2] += " → JPG + RAW"
+        else:
+            items[2] += " → JPG"
+        if(display_config["status_led"]):
+            items[3] += " → On"
+        else:
+            items[3] += " → Off"
+        menu_display("Image Settings ",items,display_config)
+
+    elif( menu >= 41 and menu <= 49 ):        # System Menu
+        items=["Screen Brightness","Disk","Wipe Data","Save Settings","Load Settings","Reset Settings"]
+        usage = 0
+        if(menu == 41):                             # Brightness Control
+            items[0] = items[0] + " → " + str(display_config["brightness"])
+
+        elif(menu == 42):                           # Disk space usage info
+            memory=shutil.disk_usage(shoot_config["storage_path"])
+            usage=int((memory[1]/memory[0])*100)
+            used=(memory[1]/1024)
+            total=(memory[0]/1024)/1024
+            if(used > 0 and used < 1024):
+                value = str(round(used,2)) + "k /" + str(round((total/1024),2)) + "g"
+            elif(used >= 1024 and used < 1048576): # and used < 1073741824):
+                value = str(round((used/1024),2)) + "m /" + str(round((total/1024),2)) + "g"
+            else:
+                value = str(round(((used/1024)/1024),2)) + "g /" + str(round((total/1024),2)) + "g"
+            items[1] = items[1] + " → " + value
+        menu_display("System Menu",items,display_config,usage)
+
+    elif( menu == 433 ):                            # Wipe data
+        items=["Screen Brightness","Disk","Wipe Data → Sure ?","Save Settings","Load Settings","Reset Settings"]
+        menu_display("System Menu",items,display_config)
+
+    elif( menu == 4333 ):                           # Wipe data confirmation
+        items=["Screen Brightness","Disk","Wipe Data → Done !","Save Settings","Load Settings","Reset Settings"]
+        menu_display("System Menu",items,display_config)
+        time.sleep(1)
+        items[2] = "Wipe Data"
+        menu_display("System Menu",items,display_config)
+
+    elif( menu == 444 ):                            # Save settings
+        items=["Screen Brightness","Disk","Wipe Data","Save Settings → Sure ?","Load Settings","Reset Settings"]
+        menu_display("System Menu",items,display_config)
+
+    elif( menu == 4444 ):                           # Save settings confirmation
+        items=["Screen Brightness","Disk","Wipe Data","Save Settings → Done !","Load Settings","Reset Settings"]
+        menu_display("System Menu",items,display_config)
+        time.sleep(1)
+        items[3] = "Save Settings"
+        menu_display("System Menu",items,display_config)
+
+    elif( menu == 455 ):                            # Load settings
+        items=["Screen Brightness","Disk","Wipe Data","Save Settings","Load Settings → Sure ?","Reset Settings"]
+        menu_display("System Menu",items,display_config)
+
+    elif( menu == 4555 ):                           # Load settings confirmation
+        items=["Screen Brightness","Disk","Wipe Data","Save Settings","Load Settings → Done !","Reset Settings"]
+        menu_display("System Menu",items,display_config)
+        time.sleep(1)
+        items[4] = "Load Settings"
+        menu_display("System Menu",items,display_config)
+
+    elif( menu == 466 ):                            # Load settings
+        items=["Screen Brightness","Disk","Wipe Data","Save Settings","Load Settings","Reset Settings → Sure ?"]
+        menu_display("System Menu",items,display_config)
+
+    elif( menu == 4666 ):                           # Load settings confirmation
+        items=["Screen Brightness","Disk","Wipe Data","Save Settings","Load Settings","Reset Settings → Done !"]
+        menu_display("System Menu",items,display_config)
+        time.sleep(1)
+        items[5] = "Reset Settings"
+        menu_display("System Menu",items,display_config)
+
+    elif( menu >= 51 and menu <= 59 ):              # Power Options
+        items=["Power Saver","Reboot","Poweroff"]
+        menu_display("Power Options",items,display_config)
+
+    elif( menu == 511 ):
+        items=["Power Saver → Sure ?","Reboot","Poweroff"]
+        menu_display("Power Options",items,display_config)
+
+    elif( menu == 522 ):
+        items=["Power Saver","Reboot → Sure ?","Poweroff"]
+        menu_display("Power Options",items,display_config)
+
+    elif( menu == 5223 ):
+        items=["Power Saver","Reboot","→ Configs saved","Poweroff"]
+        menu_display("Power Options",items,display_config)
+
+    elif( menu == 5224 ):
+        items=["Power Saver","Reboot","→ Configs saved","→ Camera stopped","Poweroff"]
+        menu_display("Power Options",items,display_config)
+
+    elif( menu == 5225 ):
+        items=["Power Saver","Reboot","→ Configs saved","→ Camera stopped","→ Rebooting now . . .","Poweroff"]
+        menu_display("Power Options",items,display_config)
+
+    elif( menu == 533 ):
+        items=["Power Saver","Reboot","Poweroff → Sure ?"]
+        menu_display("Power Options",items,display_config)
+
+    elif( menu == 5334 ):
+        items=["Power Saver","Reboot","Poweroff","→ Configs saved"]
+        menu_display("Power Options",items,display_config)
+
+    elif( menu == 5335 ):
+        items=["Power Saver","Reboot","Poweroff","→ Configs saved","→ Camera stopped"]
+        menu_display("Power Options",items,display_config)
+    elif( menu == 5336 ):
+        items=["Power Saver","Reboot","Poweroff","→ Configs saved","→ Camera stopped","→ Bye Bye !!!"]
+        menu_display("Power Options",items,display_config)
---
+++ rpi_cam.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import time
+import signal
+import RPi.GPIO as GPIO
+#import lib.LCD_2inch as LCD_2inch
+import lib.camera as camera
+import lib.display as lcd
+import lib.operations as operation
+
+LED = 17
+BUZZER = 9
+
+GPIO.setwarnings(False)
+# Setup RPI.GPIO with "BCM" numbering scheme
+GPIO.setmode(GPIO.BCM)
+# We must set the backlight pin up as an output first
+GPIO.setup(18, GPIO.OUT)
+# Set up our pin as a PWM output at 500Hz
+backlight = GPIO.PWM(18, 500)
+# Notification LED 
+GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
+# Buzzer
+GPIO.setup(BUZZER, GPIO.OUT, initial=GPIO.LOW)
+
+# Exposure time OR Shutter Speed in microseconds
+exposure_time=[0,625,800,1000,1250,1562,2000,2500,3125,4000,5000,6250,8000,10000,12500,16667,20000,25000,33333,40000,50000,66667,76923,100000,125000,166667,200000,250000,333333,400000,500000,625000,769231,1000000,1300000,1600000,2000000,2500000,3000000,4000000,5000000,6000000,8000000,10000000,13000000,15000000,20000000,25000000,30000000,60000000]
+image_height=[1600,2048,2464,3008,3264,3888,4000,4656]
+image_width=[1200,1536,1632,2000,2448,2592,2800,3496]
+
+def display_sleep(brightness,sleep):
+    if(sleep):
+        for x in range (brightness,-1,-1):
+            backlight.ChangeDutyCycle(x)
+            time.sleep(0.01)
+    else:
+        for x in range (0,brightness+1,1):
+            backlight.ChangeDutyCycle(x)
+            time.sleep(0.01)
+
+def on_LED():
+    GPIO.output(LED, GPIO.HIGH)
+
+def off_LED():
+    GPIO.output(LED, GPIO.LOW)
+
+def on_BUZZER():
+    GPIO.output(BUZZER, GPIO.HIGH)
+
+def off_BUZZER():
+    GPIO.output(BUZZER, GPIO.LOW)
+
+
+def init(display_config,shoot_config,camera_config):
+    backlight.start(1)
+    on_LED()
+    on_BUZZER()
+    lcd.boot_disp("start_logo.jpeg")
+    camera.initialize_camera(camera_config)
+    off_BUZZER()
+    display_sleep(display_config["brightness"],False)
+    off_LED()
+
+def end_program():
+    lcd.boot_disp("camera_down.png")
+    camera.stop_camera()
+    GPIO.cleanup()       # clean up GPIO on CTRL+C exit
+
+def touch_input(input_value):
+    global display_config,shoot_config,camera_config
+    if(input_value == 1):                                                             # BACK key
+        touchphat.set_led(input_value, display_config["status_led"])
+        if(display_config["menu"] == -1):
+            display_sleep(display_config["brightness"],False)
+        display_config,shoot_config,camera_config = operation.back_button(display_config,shoot_config,camera_config)
+
+    elif(input_value == 2):                                                           # Down key
+        touchphat.set_led(input_value, display_config["status_led"])
+        display_config,shoot_config,camera_config = operation.down_button(display_config,shoot_config,camera_config)
+
+    elif(input_value == 3):                                                           # Left key being used as DECREMENT ( - )
+        touchphat.set_led(input_value, display_config["status_led"])
+        display_config,shoot_config,camera_config = operation.left_button(display_config,shoot_config,camera_config)
+        if(display_config["menu"] == 41):
+            backlight.ChangeDutyCycle(display_config["brightness"])
+
+    elif(input_value == 4):                                                           # Right key being used as INCREMENT ( + )
+        touchphat.set_led(input_value, display_config["status_led"])
+        display_config,shoot_config,camera_config = operation.right_button(display_config,shoot_config,camera_config)
+        if(display_config["menu"] == 41):
+            backlight.ChangeDutyCycle(display_config["brightness"])
+
+    elif(input_value == 5):                                                           # UP key
+        touchphat.set_led(input_value, display_config["status_led"])
+        display_config,shoot_config,camera_config = operation.up_button(display_config,shoot_config,camera_config)
+
+    else:                                                                       # ENTER key
+        touchphat.set_led(input_value, display_config["status_led"])
+        if(display_config["menu"] == -1):
+            display_sleep(display_config["brightness"],False)
+        display_config,shoot_config,camera_config = operation.ok_shutter_button(display_config,shoot_config,camera_config)
+        if(((display_config["menu"] > 40) and (display_config["menu"] < 50)) or ((display_config["menu"] > 4000) and (display_config["menu"] < 5000))):
+            backlight.ChangeDutyCycle(display_config["brightness"])
+        if(display_config["menu"] == -2):
+            display_config["menu"] = -1
+            display_sleep(display_config["brightness"],True)
+
+###############################################################################################################
+# main()
+###############################################################################################################
+
+def main():
+    global display_config,shoot_config,camera_config
+    display_config,shoot_config,camera_config = operation.load_settings("auto_saved")
+    init(display_config,shoot_config,camera_config)
+    try:
+        lcd.camera_home(display_config,shoot_config,camera_config,camera.shoot_preview(camera_config))
+        signal.pause()  
+    except KeyboardInterrupt:
+        print("Keyboard interrupt detected")
+        display_config["menu"] == 0
+        operation.save_settings(display_config,shoot_config,camera_config,"auto_saved")
+        print("Current settings saved")
+        end_program()
+
+if __name__ == '__main__':
+    main()
